a355e1781214650948e076dff85dd7c8284983b1
Ritik Kumar <149877642+ritikkumar95@users.noreply.github.com>
2024-05-08 14:56:32 -0300

    Update portfolio.ts

--- portfolio.ts
+++ portfolio.ts
@@ -101,8 +101,8 @@ export const skillsSection: SkillsSectionType = {
 
 export const SkillBars: SkillBarsType[] = [
   {
-    Stack: "Frontend/Design", //Insert stack or technology you have experience in
-    progressPercentage: "85", //Insert relative proficiency in percentage
+    Stack: "Frontend/Design",
+    progressPercentage: "85", 
   },
   {
     Stack: "Backend",
@@ -125,34 +125,7 @@ export const educationInfo: EducationType[] = [
   },
 ];
 
-// export const experience: ExperienceType[] = [
-  
-//   {
-//     role: "Full Stack Developer",
-//     company: "Duseca Software",
-//     companyLogo: "/img/icons/common/duseca_software_logo.jpeg",
-//     date: "Jan 2022 - Sept 2023",
-//     desc: "Crafted robust mobile application backends employing Django, Python, and REST APIs. Leveraged Django REST Framework, PostgreSQL, AWS, Firebase, Stripe, and WebSocket for efficient development. Collaborated across teams to surpass client expectations. Designed intuitive UIs, empowering clients to effortlessly manage their applications with a visually captivating interface.",
-//     // descBullets: [
-//     // 	"Lorem ipsum dolor sit amet, consdfgectetur adipiscing elit",
-//     // 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-//     // ],
-//   },
-//   {
-//     role: "Full Stack Developer",
-//     company: "ZRTechnologies",
-//     companyLogo: "/img/icons/common/zrtech.jpeg",
-//     date: "Aug 2022 - Jun 2023",
-//     desc: "Crafting diverse web applications, I've elevated functionality and user engagement. Spearheaded Visual Portfolio, leveraging AI for captivating presentations from parsed text. Proficiently set up and optimized applications on Linux servers, guaranteeing smooth scalability and top-notch performance for seamless user experiences.",
-//   },
-//   {
-//     role: "Backend Developer",
-//     company: "Bleed-AI",
-//     companyLogo: "/img/icons/common/bleedAI.jpg",
-//     date: "Sept 2021 - Oct 2021",
-//     desc: "As a Django developer at Bleed AI, I integrated a computer vision AI model to process YouTube URLs on a website. I used Django Channels and Websockets to show real-time processing and implemented threading to run multiple instances of the AI model. I also used jQuery for UI rendering and deployed the website on a Heroku server.",
-//   },
-// ];
+
 
 export const projects: ProjectType[] = [
   
@@ -174,41 +147,19 @@ export const projects: ProjectType[] = [
     github: "https://github.com/1hanzla100/developer-portfolio",
     link: "#",
   },
-  // {
-  //   name: "Hooligan Culture",
-  //   desc: "A Ecommerce Platform, where shopping meets the future! With HandCash BitcoinSV integration, you can authenticate and purchase products with ease and confidence.",
-  //   link: "https://hooliganculture.com/",
-  // },
+  
 ];
 
-// export const feedbacks: FeedbackType[] = [
-//   {
-//     name: "Syed Jamal",
-//     role: "Frontend Developer at Meganos Software",
-//     feedback:
-//       "You were a wonderful addition to our team, alhumdulillah. I'm quite impressed with your abilities and professional practices. You approach problems from a unique perspective and always ask the right question. Finding a good teammate like Hanzla is extremely difficult. He has contributed to the growth of our organization and continues to do so. May Allah bless you with everything you desire.",
-//   },
-//   {
-//     name: "Wajahat Malek",
-//     role: "CEO at Duseca Software",
-//     feedback:
-//       "Hanzla has a deep understanding of web development technologies and is adept at using them to build scalable, robust, and secure web applications. He has worked on various projects, ranging from small-scale to large-scale, and has shown his ability to work collaboratively with others while maintaining a high level of productivity and quality. Hanzla is a pleasure to work with, and I have no doubt that he will be an asset to any team or organization. I highly recommend Hanzla for any Full Stack Django, React.js, or Blockchain development projects, and I wish him all the best in his future endeavors.",
-//   },
-//   {
-//     name: "Zaid Zaffar",
-//     role: "CEO at ZR Technologies",
-//     feedback:
-//       "Very skilled at what he does and has an in depth knowledge of Django and he's always ready to handle any challenges thrown at him. He's been a valuable member of my team and i look forward to working with him in the years to come",
-//   },
-// ];
 
-// See object prototype on /types/section.ts page
+
+
 export const seoData: SEODataType = {
   title: "Ritik kumar",
   description: greetings.description,
   author: "Ritik kumar",
-  image: "https://avatars.githubusercontent.com/u/59178380?v=4",
-  url: "https://developer-portfolio-1hanzla100.vercel.app",
+  image: "https://avatars.githubusercontent.com/ritikkumar95",
+
+  url: "https://main--portfolio-of-ritik.netlify.app/",
   keywords: [
     "Ritik",
     "Ritik kumar",
